352cad7830974db22307641174da28b256bb724e
Marc DeXeT <marc.dexet@ias.u-psud.fr>
2019-12-03 22:20:57 +0100

    Solution Bean EnergyTank 1
    Use annotated static factory method with
    
    * Static factory with @Component annotation
    * @bean annot. upon static method

--- src/main/java/ex/energy/EnergyTankStaticFactory.java
+++ src/main/java/ex/energy/EnergyTankStaticFactory.java
@@ -1,7 +1,12 @@
 package ex.energy;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EnergyTankStaticFactory {
 	
+	@Bean
 	public static EnergyTank getDefaultTank() {
 		System.out.println(" #-> EnergyTankStaticFactory->getDefaultTank()");
 		return new EnergyTank(1000);
