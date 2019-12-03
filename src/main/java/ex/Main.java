4cd80f853fcb7b64fd89663da9b1fef155067e4c
Marc DeXeT <marc.dexet@cnrs.fr>
2019-12-03 17:45:27 +0100

    Solution bean phase 1

--- src/main/java/ex/Main.java
+++ src/main/java/ex/Main.java
@@ -8,7 +8,7 @@ import ex.config.MainConfig;
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(MainConfig.class);
+		ApplicationContext context = new AnnotationConfigApplicationContext(MainConfig.class);		
 		Robot robot = context.getBean(Robot.class);
 		robot.run();
 	}
--- src/main/java/ex/config/MainConfig.java
+++ src/main/java/ex/config/MainConfig.java
@@ -1,8 +1,38 @@
 package ex.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import ex.Robot;
+import ex.command.CommandUnit;
+import ex.energy.EnergySupplier;
+import ex.radio.RadioTransmitter;
 
 @Configuration
 public class MainConfig {
 
+    @Bean
+    public RadioTransmitter transmitter() {
+        System.out.println(".....@Bean RadioTransmitter transmitter()");
+        return new RadioTransmitter();
+    }
+    
+    @Bean
+    public EnergySupplier energySupplier() {
+        System.out.println(".....@Bean EnergySupplier energySupplier");
+        return new EnergySupplier();
+    }
+    
+    @Bean
+    public CommandUnit commandUnit() {
+        System.out.println(".....@Bean CommandUnit commandUnit");
+        return new CommandUnit();
+    }  
+    
+    @Bean 
+    public Robot robot(CommandUnit commandUnit, RadioTransmitter transmitter, EnergySupplier energySupplier) {
+    	System.out.println(".....@Bean Robot robot");
+    	Robot robot = new Robot(commandUnit, transmitter, energySupplier);
+    	return robot;
+    }
 }
