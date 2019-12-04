1dfef4149d4f7402df2bac9ee80fd4bcd677c0b2
Marc DeXeT <marc.dexet@cnrs.fr>
2019-12-04 10:04:35 +0100

    Solution exercise 6

--- src/main/java/ex/config/MainConfig.java
+++ src/main/java/ex/config/MainConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import ex.energy.EnergyTank;
+import ex.energy.NuclearEnergyTank;
 
 @Configuration
 @ComponentScan("ex")
@@ -15,9 +16,15 @@ import ex.energy.EnergyTank;
 	})
 public class MainConfig {
 
+	
 	@Bean
-	public EnergyTank energyTank(@Value("${initial.quantity:1000}") float initialQuantity) {
-		return new EnergyTank(initialQuantity);
-	}
+	public EnergyTank energyTank(@Value("${initial.quantity:1000}") float initialQuantity, @Value("${energy.kind}") String mode) {
+		if ( "classic".equals(mode)) {
+			return new EnergyTank(initialQuantity);
+		} else if ( "nuclear".equals(mode)) {
+			return new NuclearEnergyTank(initialQuantity);
+		}
+		throw new IllegalArgumentException("Mode "+mode+" is not available");
+	}	
 	
 }
